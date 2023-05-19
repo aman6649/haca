c4e0ef3215489667bbfaf399b5f35f1a7a17df28
aman6649 <aman.ag6649@gmail.com>
2023-05-19 11:20:12 +0530

    almost done

--- client/src/components/Newsletter.js
+++ client/src/components/Newsletter.js
@@ -2,24 +2,27 @@ import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
 
 export const Newsletter = ({ status, message, onValidated }) => {
-  const [email, setEmail] = useState("");
+  const handleDownload = () => {
+    const pdfUrl =
+      "https://drive.google.com/file/d/1ZSdwSmv3W6yvGYxtK6EXJmyaIOPvdfM1/view?usp=share_link";
+    window.open(pdfUrl, "_blank");
+  };
+
+  const [isLaptopView, setIsLaptopView] = useState(window.innerWidth >= 992);
 
   useEffect(() => {
-    if (status === "success") clearFields();
-  }, [status]);
+    const handleWindowResize = () => {
+      setIsLaptopView(window.innerWidth >= 992);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    email &&
-      email.indexOf("@") > -1 &&
-      onValidated({
-        EMAIL: email,
-      });
-  };
+    window.addEventListener("resize", handleWindowResize);
 
-  const clearFields = () => {
-    setEmail("");
-  };
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const buttonStyle = isLaptopView ? { marginLeft: "100px" } : {};
 
   return (
     <Col lg={12}>
@@ -30,26 +33,20 @@ export const Newsletter = ({ status, message, onValidated }) => {
               For more detailed information about the HackFest, please refer to
               the attached PDF document.
             </h3>
-            {status === "sending" && <Alert>Sending...</Alert>}
-            {status === "error" && <Alert variant="danger">{message}</Alert>}
-            {status === "success" && <Alert variant="success">{message}</Alert>}
           </Col>
           <Col md={6} xl={7}>
-            {/* <form onSubmit={handleSubmit}> */}
-            {/* <div className="new-email-bx"> */}
-            {/* <input
-                  value={email}
-                  type="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email Address"
-                /> */}
-            <button type="submit">Download</button>
-            {/* </div> */}
-            {/* </form> */}
+            <div className="new-email-bx">
+              <button
+                type="button"
+                onClick={handleDownload}
+                style={buttonStyle}
+              >
+                Download
+              </button>
+            </div>
           </Col>
         </Row>
       </div>
     </Col>
-    // <></>
   );
 };
